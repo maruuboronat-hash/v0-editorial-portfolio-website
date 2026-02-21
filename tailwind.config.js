ef930b0d67fd2dfb293c3c49aa8d5717a2060d32
maruuboronat-hash <maruuboronat@gmail.com>
2026-02-20 21:07:35 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,17 @@
-
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        heading: ["var(--font-heading)", "sans-serif"],
+        sans: ["var(--font-body)", "sans-serif"],
+        museo: ["var(--font-museo)", "sans-serif"], // ← AGREGÁ ESTO
+      },
+    },
+  },
+  plugins: [],
+};
