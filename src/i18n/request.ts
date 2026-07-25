411df1f8bf5abab7de6d7fdf3343608745dcdd9c
maruuboronat-hash <maruuboronat@gmail.com>
2026-07-25 00:31:44 -0300

    Delete src directory

--- src/i18n/request.ts
+++
@@ -1,9 +0,0 @@
-import { getRequestConfig } from 'next-intl/server';
-
-export default getRequestConfig(async () => {
-  const locale = 'es';
-  return {
-    locale,
-    messages: (await import(`../../messages/${locale}/common.json`)).default
-  };
-});
