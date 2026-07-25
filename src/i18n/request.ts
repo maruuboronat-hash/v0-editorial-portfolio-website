00dfd9e23292f9e0489183bd48af9e5479ecf542
maruuboronat-hash <maruuboronat@gmail.com>
2026-07-25 00:20:01 -0300

    Update request.ts

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,5 +1,11 @@
 import { getRequestConfig } from 'next-intl/server';
- 
-export default getRequestConfig(async ({ locale }) => ({
-  messages: (await import(`../../messages/${locale}/common.json`)).default
-}));
+
+export default getRequestConfig(async () => {
+  // Por ahora usamos español por defecto
+  const locale = 'es';
+
+  return {
+    locale,
+    messages: (await import(`../../messages/${locale}/common.json`)).default
+  };
+});
